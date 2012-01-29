5e2ab3365617fbafd3ca4a8fb84b295c93a5acfa
dbarnhart <darryl@dbarnhart.ca>
2012-01-29 01:25:55 -0500

    Convert app to globe game

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,6 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1', text:'Sample App') }
     it { should have_selector('title', text: full_title('Home')) }
   end
 
